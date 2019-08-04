c8eaf47aade2e69c338326d8c7e1e77c9c427682
George Murphy <george@websbygeorge.onmicrosoft.com>
2019-08-04 17:19:27 -0400

    Updated shell script for what I needed

--- InstallCountChocula.ps1
+++ InstallCountChocula.ps1
@@ -2,13 +2,15 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
 #Assign Packages to Install
 $Packages = 'googlechrome',`
-            'visualstudiocode',`
-            'git',`
-            'visualstudio2017community',`
-            'visualstudio2017-workload-azure',`
-            'visualstudio2017-workload-netweb'
+            'vscode',`
+            'git.install',`
+            'vscode-docker',`
+            'conemu',`
+            'docker-desktop'
 
 #Install Packages
 ForEach ($PackageName in $Packages)
