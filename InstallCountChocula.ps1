95aff3bc4d49175a5b44dc8d59978be2e91a0dc7
George Murphy <georgemurphy@websbygeorge.com>
2019-09-09 20:12:35 -0400

    Added Commandbox

--- InstallCountChocula.ps1
+++ InstallCountChocula.ps1
@@ -5,6 +5,7 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 #Assign Packages to Install
 $Packages = 'googlechrome',`
             'vscode',`
+            'commandbox',`
             'git.install',`
             'vscode-docker',`
             'conemu',`
