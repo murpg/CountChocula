96dadde97e0792cdbfe14668072d1102422a269d
George Murphy <georgemurphy@websbygeorge.com>
2019-11-25 11:27:05 -0500

    updated powershell script to force it to run

--- setUpIIS.ps1
+++ setUpIIS.ps1
@@ -4,7 +4,7 @@
 # * Make sure you run this script from a Powershel Admin Prompt!
 # * Make sure Powershell Execution Policy is bypassed to run these scripts:
 # * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
-Set-ExecutionPolicy Bypass -Scope Process
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
 # To list all Windows Features: dism /online /Get-Features
 # Get-WindowsOptionalFeature -Online 
