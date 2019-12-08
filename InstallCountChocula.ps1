545b1c9b1265da434c9c9f7c70d490c3519f99c8
George Murphy <georgemurphy@websbygeorge.com>
2019-12-07 22:18:56 -0500

    Added the correct Module to Remove the Default web site

--- InstallCountChocula.ps1
+++ InstallCountChocula.ps1
@@ -16,7 +16,8 @@ ForEach ($PackageName in $Packages)
 
 #Reboot
 Add-WindowsFeature Web-Asp-Net45;Add-WindowsFeature NET-Framework-45-Core;Add-WindowsFeature Web-Net-Ext45;Add-WindowsFeature Web-ISAPI-Ext;Add-WindowsFeature Web-ISAPI-Filter;Add-WindowsFeature Web-Mgmt-Console;Add-WindowsFeature Web-Scripting-Tools;Add-WindowsFeature Search-Service;Add-WindowsFeature Web-Filtering;Add-WindowsFeature Web-Basic-Auth;Add-WindowsFeature Web-Windows-Auth;Add-WindowsFeature Web-Default-Doc;Add-WindowsFeature Web-Http-Errors;Add-WindowsFeature Web-Static-Content;
-Remove-IISSite -Name "Default Web Site";
+Import-Module WebAdministration;
+Remove-Wesite -Name "Default Web Site";
 New-Item C:\inetpub\wwwroot\websites\pmses -ItemType Directory;
 New-WebSite -Name pmses -Port 80 -HostHeader pmses.devmilnet.com -PhysicalPath "$env:systemdrive\inetpub\wwwroot\websites\pmses";
 choco install urlrewrite -y;
