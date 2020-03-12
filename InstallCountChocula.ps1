f1f848b6ab0d53680ad38f65068767a965865b49
George Murphy <georgemurphy@websbygeorge.com>
2020-03-11 20:43:05 -0400

    fixed a tls problem

--- InstallCountChocula.ps1
+++ InstallCountChocula.ps1
@@ -1,6 +1,7 @@
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
 $Packages = 'googlechrome',`
